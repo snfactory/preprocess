0bd2e6a658984cd045b5b7b7472c1bf31a0359bf
ycopin <ycopin>
2007-10-25 22:21:44 +0000

    new option '-C' to use an input catalog.

--- pkg/pipeline/quick/quick_bias.sh
+++ pkg/pipeline/quick/quick_bias.sh
@@ -6,7 +6,7 @@
 ## Id:            $Id$
 ######################################################################
 
-# arg : we should have a list of files
+# arg : we should have a list of files (or a catalog)
 # they will be processed
 # then a bias will be computed out of them
 
@@ -20,24 +20,23 @@ function usage() {
 Program: quick_bias  Version $version
 
 Available options:
- -o outbias [-m maximages] [-f][-d][-h][-v] bias1.fits bias2.fits [bias*.fits]
+ -o outbias [-C incat] [-m maximages] [-f][-d][-h][-v] [bias1.fits [bias2.fits ...]]
 
 where:
     outbias : Output bias name.
-  maximages : The maximum number of images which may be simultaneously
-              present in the memory.
-	      The higher, the better performance - but the higher chance to
-	      overflow the RAM.
-	      Note that it is not the max number of input images. This one can 
-              be very high (up to 1000 should go fine).
+      incat : Input catalog. Can replace multiple arguments.
+  maximages : The maximum number of images which may be simultaneously present
+              in the memory.  The higher, the better performance - but the
+              higher chance to overflow the RAM.  Note that it is not the max
+              number of input images. This one can be very high (up to 1000
+              should go fine).
 	      default : 7 (corresponding to approx 1GB RAM)
      f-flag : Do not ask before overwriting files.
      d-flag : Debug mode
 
-Example:
-
+Examples:
     quick_bias -o bias_B.fits -f *_24_B.fits
-
+    quick_bias -o bias_B.fits -C mybiases_B.cat
 EOF
 }
 
@@ -50,16 +49,16 @@ function die() {
 }
 
 
-
 # ############################################################
 
 # Options ==============================
 
 # Parser ..............................
-while getopts "o:m:fdvh" OPTION ; do
+while getopts "o:m:C:fdvh" OPTION ; do
     case "$OPTION" in
         o) outbias="$OPTARG" ;; # Output datacube
 	m) maximages="$OPTARG" ;; # max space for processing
+	C) catalog="$OPTARG" ;;   # Input catalog
         f) noask="-noask" ;;    # Do not ask before overwriting files
 	d) DEBUG="-d";echo "*** DEBUG mode ***";;
         v) echo "$version" ; exit 0 ;;
@@ -68,37 +67,53 @@ while getopts "o:m:fdvh" OPTION ; do
     esac
 done
 
+# Read arguments
 shift $(($OPTIND-1))
 inframes=$@
 
-if [ -z "$inframes" ] || [ -z $outbias ]; then
-    die 1 $LINENO "Arguments of options -o missing or no input files"
+if [ -z "$inframes" ] && [ -z $catalog ]; then
+    die 1 $LINENO "No input files or option -C missing."
 fi
-if [ -e Pfiles.cat ] ; then
-    if [ -n "$noask" ] ; then
-	rm -f Pfiles.cat
-    else 
-	die 1 $LINENO "Pfiles.cat already exists - is a concurrent processing runing on this directory?"
-    fi
+if [ -z $outbias ]; then
+    die 1 $LINENO "Option -o missing."
 fi
+
+# Create input catalog if needed
+if [ -z "$catalog" ]; then
+    incat=qb_$$.cat
+    echo "# quick_bias $version -- $(date)" > $incat
+    echo $inframes | tr ' ' '\n' >> $incat
+    [ $DEBUG ] && echo "Creating input catalog $incat"
+else
+    incat=$catalog
+    [ $DEBUG ] && echo "Using input catalog $incat"
+fi
+
 ### Translate the number of images in number of lines for 1 image.
 # We have to hold in fact ALL images in memory.
-nlines=$(echo "4096*$maximages/($(echo $inframes | wc -w)+1)" | bc)
+nframes=$(cat $incat | grep -v '^#' | wc -l)
+nlines=$(echo "4096*$maximages/($nframes+1)" | bc)
 
-cat <<EOF > Pfiles.cat
-#### Processed files catalog
-EOF
+# Create preprocess output catalog
+outcat=Pqb_$$.cat
+[ $DEBUG ] && echo "Creating output catalog $outcat"
+cat $incat | sed -e 's/^\([^#]\)/P\1/' > $outcat
 
 ### Actual processing here
 
-for file in $inframes ; do
-    Pfile=`echo $file | sed 's%.*/%% ; s%^%P%'`
-    echo $Pfile >> Pfiles.cat
-    [ $DEBUG ] && echo "preprocess -in $file -out $Pfile"
-    preprocess -in $file -out $Pfile $noask
-done
-[ $DEBUG ] && echo "stack_image -in Pfiles.cat -out $outbias -nlines $nlines"
-stack_image -in Pfiles.cat -out $outbias -nlines $nlines $noask
-rm -f Pfiles.cat
+[ $DEBUG ] && echo "preprocess -in $incat -out $outcat"
+preprocess -in $incat -out $outcat $noask
+
+[ $DEBUG ] && echo "stack_image -in $outcat -out $outbias -nlines $nlines"
+stack_image -in $outcat -out $outbias -nlines $nlines $noask
+
+# Cleaning
+if [ $DEBUG ]; then
+    [ -z "$catalog" ] && echo "DEBUG: Catalog $incat not removed."
+    echo "DEBUG: Catalog $outcat not removed."
+else
+    [ -z "$catalog" ] && rm -f $incat
+    rm -f $outcat
+fi
 
 exit 0
